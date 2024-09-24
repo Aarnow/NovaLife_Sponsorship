5496a4bad1de3a452ab8d8f524f0438a499c8c5c
Aarnow <arnaudfgr.dev@gmail.com>
2024-09-24 06:20:20 +0200

    Initial Project Files

---
+++ Sponsorship.cs
@@ -0,0 +1,23 @@
+﻿using Life;
+using ModKit.Helper;
+using ModKit.Interfaces;
+
+namespace Sponsorship
+{
+    public class Sponsorship : ModKit.ModKit
+    {
+       
+        public Sponsorship(IGameAPI api) : base(api)
+        {
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Aarnow");
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+           
+
+            ModKit.Internal.Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+    }
+}
